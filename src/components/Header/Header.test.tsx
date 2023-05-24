f375405d309d80f13c7723dbe2d8357e93bcc1d8
limoza <svacc.nelllely@gmail.com>
2023-05-24 23:18:06 +0900

    Headerコンポーネントの作成、テストの作成

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react'
+import { Header } from './Header'
+
+describe('Header components Test', () => {
+  it('Ensure that there is only one header', () => {
+    render(<Header />)
+    expect(screen.getAllByRole('banner')).toHaveLength(1)
+  })
+
+  it('Ensure that there is only one h1', () => {
+    render(<Header />)
+    expect(screen.getAllByRole('heading', { level: 1 })).toHaveLength(1)
+  })
+
+  it('should display the correct text', () => {
+    render(<Header />)
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+        name: '都道府県別 総人口推移グラフ',
+      })
+    ).toBeInTheDocument()
+  })
+})
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header>
+      <h1>都道府県別 総人口推移グラフ</h1>
+    </header>
+  )
+}
